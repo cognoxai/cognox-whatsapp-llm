8ce4c68a6d69258fb3fbca3822d9361872c2f04a
cognoxai <cognox.ai@gmail.com>
2025-07-14 18:37:57 -0300

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,8 @@
-from ..database import db  # Importa 'db' do novo arquivo database.py
+from src.database import db
 from datetime import datetime
 
 class User(db.Model):
+    # ... (o resto do arquivo permanece o mesmo)
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
