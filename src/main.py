e93934a0d86e8bc6e65ae7937dd638f3ea214480
cognoxai <cognox.ai@gmail.com>
2025-07-14 12:45:31 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -37,7 +37,15 @@ def serve(path):
     if static_folder_path is None:
             return "Static folder not configured", 404
 
-    if path
+    if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
+        return send_from_directory(static_folder_path, path)
     else:
         index_path = os.path.join(static_folder_path, 'index.html')
+        if os.path.exists(index_path):
+            return send_from_directory(static_folder_path, 'index.html')
+        else:
+            return "index.html not found", 404
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
